5d95c8e04bd87751adce6be1303c469d2b1bcdf3
andres <andresrlista@gmail.com>
2024-05-15 08:35:44 -0400

    fixed cors nro. 2

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -6,7 +6,12 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    origin: '*',
+    origin: [
+      'http://localhost:3030',
+      'http://0.0.0.0:3030',
+      'https://localhost:3030',
+      'https://0.0.0.0:3030',
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
   })
 );
