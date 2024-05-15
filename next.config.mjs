54268f5380b7f968d8a215fe9585bb166c18cf4a
andres <andresrlista@gmail.com>
2024-05-15 01:31:18 -0400

    fixed cors error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     DEV_API: 'http://localhost:3000',
-    PRODUCTION_API: 'https://your-domain-api.vercel.app',
+    PRODUCTION_API: 'https://minimal-api-dev-green.vercel.app/',
   },
 };
 
--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -6,7 +6,7 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    origin: '*',
+    origin: ['http://localhost:3030', 'https://minimal-api-dev-green.vercel.app'],
     methods: ['GET', 'POST', 'OPTIONS'],
   })
 );
